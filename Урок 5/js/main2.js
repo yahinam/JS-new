f4fe6031e41dcff8b5545cce75d52115d9ace747
yahinam <yahinam@mail.ru>
2020-07-12 23:11:43 +0300

    Внесла изменения с учетом замечаний Артема

--- Урок 5/js/main2.js
+++ Урок 5/js/main2.js
@@ -4,10 +4,11 @@ const app = new Vue({
     el: "#app",
     data: {
         catalogUrl: '/catalogData.json',
+        cartUrl: '/getBasket.json',
         products: [],
         imgCatalog: 'https://placehold.it/200x150',
+        imgCart: 'https://placehold.it/50x100',
         filtered: [],
-        regexp: '',
         searchLine: '',
         cart: [],
         seen: true,
@@ -15,10 +16,8 @@ const app = new Vue({
         totalPrice: 0,
         show: false,
         cartCount: '',
-        invisible: {display: 'none'},
-        visible: true
-
     },
+
     methods: {
         getJson(url) {
             return fetch(url)
@@ -28,48 +27,67 @@ const app = new Vue({
                 })
         },
 
-        addProduct(product){                    
-            if (this.cart.includes(product)) {
-                product.quantity ++;
-                product.totalPrice = product.quantity * product.price;
-            } else {
-                this.cart.push(product);
-                Vue.set(product, 'quantity', 1);
-                Vue.set(product, 'totalPrice', product.price);
-            }
-            this.cartCount++;
+        addProduct(product){
+            this.getJson(`${API}/addToBasket.json`)
+                .then(data => {
+                if (data.result === 1) { 
+                    let find = this.cart.find(el => el.id_product === product.id_product); //см. комм-й стр. 40              
+                    if (find) {
+                        find.quantity ++;
+                        // find.totalPrice = find.quantity * find.price;
+                    } else {
+                        let prod = Object.assign({quantity: 1}, product); // создается новый объект, потому что изменять product нельзя, он относится к другой сущности (продуктлист), что может привести к искажению входных данных
+                        this.cart.push(prod);
+                    }
+                    this.cartCount++;
+                } else {
+                    alert('Error');
+                }
+            })
         },
 
-        deleteProductFromCart(product) {
-            if (product.quantity !== 1) {
-                product.quantity --;
-                product.totalPrice = product.quantity * product.price;
-            } else {
-                let index = this.cart.indexOf(product);
-                this.cart.splice(index, 1);
-            }
-            this.cartCount--;
-            if (this.cartCount === 0) {
-                this.cartCount = '';
-            }
+        removeProductFromCart(item) {
+            this.getJson(`${API}/deleteFromBasket.json`)
+                .then(data => {
+                    if (data.result === 1) {
+                        if (item.quantity > 1) {
+                            item.quantity --;
+                            // item.totalPrice = item.quantity * item.price;
+                            this.cartCount--;
+                        } else {
+                            let index = this.cart.indexOf(item);
+                            this.cart.splice(index, 1); // лучше this.cart.splice(this.cart.indexOf(item), 1)
+                        }
+                        // if (this.cartCount === 0) {
+                        //     this.cartCount = '';
+                        // }
+                    }
+                })
         },
 
         searchProduct(value) {
-            regexp = new RegExp(value, 'i');
-            this.filtered = this.products.filter(good => regexp.test(good.product_name));
+            let regexp = new RegExp(this.searchLine, 'i');
+            this.filtered = this.products.filter(el => regexp.test(el.product_name));
         },
     },
 
     computed: {
     },
 
-    created() {
+    mounted() {
         this.getJson(`${API + this.catalogUrl}`)
             .then(data => {
                 for(let el of data){
                     this.products.push(el);
+                    this.filtered.push(el);
                 }
-                this.filtered = this.products;
             });
+        
+        this.getJson(`${API + this.cartUrl}`)
+            .then(data => {
+              for (let el of data.contents) {
+                this.cart.push(el);
+              }
+        });
     },
 })
